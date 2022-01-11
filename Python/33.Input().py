bb47f4ab68c79f4c1ad83b5bb09a294b90fb0157
M.ROHAN FAROOQUI <31238460+RohanFarooqui@users.noreply.github.com>
2022-01-11 09:38:54 +0500

    Hacker Rank - Python

--- Python/33.Input().py
+++ Python/33.Input().py
@@ -44,3 +44,9 @@ P(1) = 1^3 + 1^2 + 1 + 1 = 4 = k
 Hence, the output is True.
 
 '''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+x, k = map(int,input().split(" "))
+print(eval(input()) == k)
